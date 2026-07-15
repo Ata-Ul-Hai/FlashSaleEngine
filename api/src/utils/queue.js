eed5d5efa39217fc5f731ed0849eb91ce4665881
Ata-Ul-Hai <ataulhai2007@gmail.com>
2026-07-16 01:00:02 +0530

    wrote the logic for BullMQ connection

---
+++ api/src/utils/queue.js
@@ -0,0 +1,22 @@
+import { Queue } from 'bullmq';
+
+const connection = {
+    host: process.env.REDIS_HOST || 'redis',
+    port: 6379
+};
+
+export const checkoutQueue = new Queue('checkout-queue', {
+    connection
+});
+
+export const addToCheckoutQueue = async (jobData) => {
+    await checkoutQueue.add('checkout-job', jobData, {
+        removeOnComplete: true,
+        removeOnFail: false, //* kept in the list if failed to debug later!
+        attempts: 3, // Number of retry attempts
+        backoff: {
+            type: 'exponential',
+            delay: 1000 // Delay in milliseconds before retrying
+        }
+    });
+};
